032be0d223bfaca58ba9b69c4d9b0a9649ec8d08
Shipra Verma <vaaishpra19@gmail.com>
2020-10-14 12:37:21 +0530

    progresses

--- Cutisey/urls.py
+++ Cutisey/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path("", views.index, name="CutiseyHome")
+    path("", views.index, name="CutiseyHome"),
+    path("about/", views.about, name="AboutUs"),
+    path("contact/", views.contact, name="ContactUs"),
+    path("tracker/", views.tracker, name="TrackingStatus"),
+    path("search/", views.search, name="Search"),
+    path("checkout/", views.checkout, name="Checkout"),
+    path("productView/", views.productView, name="ProductView")
 ]
--- Cutisey/views.py
+++ Cutisey/views.py
@@ -2,5 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'Cutisey/index.html')
+
+
+def about(request):
+    return render(request, 'Cutisey/about.html')
+
+
+def contact(request):
+    return render(request, 'Cutisey/contact.html')
+
+def tracker(request):
+    return render(request, 'Cutisey/tracker.html')
+    
+def search(request):
+    return render(request, 'Cutisey/search.html')
+
+def checkout(request):
+    return render(request, 'Cutisey/checkout.html')
+
+def productView(request):
+    return render(request, 'Cutisey/prodView.html')
+
