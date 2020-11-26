083d42e73ccdd50b7f65fcbdedbb6d7437d3fb64
Shipra Verma <vaaishpra19@gmail.com>
2020-11-26 13:15:49 +0530

    contact page

--- Cutisey/views.py
+++ Cutisey/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product,Contact
 from math import ceil
 
 # Create your views here.
@@ -44,5 +44,6 @@ def contact(request):
         email = request.POST.get('email','')
         phone = request.POST.get('phone','')
         desc = request.POST.get('desc','')
-        print(name, email, phone, desc)
+        contact = Contact(name=name, email=email, phone=phone, desc=desc)
+        contact.save()
     return render(request, 'Cutisey/contact.html')
