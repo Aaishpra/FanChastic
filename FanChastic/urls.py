c2b2400733a98c9655195f8850a8f62c239e0fc1
Shipra Verma <vaaishpra19@gmail.com>
2020-12-05 06:22:39 +0530

    Home page created

--- FanChastic/urls.py
+++ FanChastic/urls.py
@@ -17,12 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from Cutisey import views
 
 urlpatterns = [
     
     path('admin/', admin.site.urls),
     path('Cutisey/', include('Cutisey.urls')),
-    path('blog/', include('blog.urls')),
-    
+    path('', include('blog.urls')),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
